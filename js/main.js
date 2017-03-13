f9da70a13bfb01d77eed7d643423da408892d9ed
Jiun Wang <jiuntwang@gmail.com>
2017-03-13 02:35:33 -0400

    Added JS and documentation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Hide and show Flickr Gallery
 $(function() {
    $("button").click(function(){
       $("#flickrembed").animate({
@@ -5,3 +6,22 @@ $(function() {
       });
    });
 });
+
+
+//Featurettes button action
+document.getElementById("myBtn").addEventListener("click", function(){
+    document.getElementById("demo").innerHTML = "Purrrrfect!";
+});
+
+document.getElementById("myBtn2").addEventListener("click", function(){
+    document.getElementById("demo2").innerHTML = "I look furrrasticly good!";
+});
+
+
+//Pop Up Event Listener, page not created
+var x = document.getElementById("popUp");
+x.addEventListener("click", myFunction);
+
+function myFunction() {
+    alert ("Opps! This page isn't created yet.")
+}
